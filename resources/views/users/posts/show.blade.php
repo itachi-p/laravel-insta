9749629f54c52ea6fe0b5dd41384613103cf50b7
Taichi Shinohara <itachi-p@Taichis-MacBook-Pro.local>
2024-09-11 08:57:11 +0900

    feat: Add details to show page

--- resources/views/users/posts/show.blade.php
+++ resources/views/users/posts/show.blade.php
@@ -16,9 +16,10 @@
                     <div class = "row align-items-center">
                         {{-- avatar --}}
                         <div class = "col-auto">
-                        <a   href  = "#">
+                            <a href  = "#">
                                 @if ($post->user->avatar)
-                                    <img src="{{ $post->user->avatar }}" alt="{{ $post->user->name }}" class="rounded-circle avatar-sm">
+                                    <img src="{{ $post->user->avatar }}" alt="{{ $post->user->name }}"
+                                        class="rounded-circle avatar-sm">
                                 @else
                                     <i class="fa-solid fa-circle-user text-secondary icon-sm"></i>
                                 @endif
@@ -27,17 +28,87 @@
 
                         {{-- name --}}
                         <div class = "col ps-0">
-
+                            <a href  = "#" class = "text-decoration-none text-dark">
+                                {{ $post->user->name }}
+                            </a>
                         </div>
 
                         {{-- Action Buttons --}}
                         <div class = "col-auto">
+                            {{-- If you are the owner of th post, you can edit or delete this post --}}
+                            @if (Auth::user()->id === $post->user->id)
+                                <button class = "btn btn-sm shadow-none" data-bs-toggle = "dropdown">
+                                    <i class="fa-solid fa-ellipsis"></i>
+                                </button>
 
+                                <div class = "dropdown-menu">
+                                    {{-- edit --}}
+                                    <a href  = "#" class = "dropdown-item">
+                                        <i class = "fa-regular fa-pen-to-square"></i> Edit
+                                    </a>
+
+                                    {{-- delete --}}
+                                    <button class = "dropdown-item text-danger" data-bs-toggle = "modal"
+                                        data-bs-target = "#delete-post-{{ $post->id }}">
+                                        <i class="fa-regular fa-trash-can"></i> Delete
+                                    </button>
+                                </div>
+                                {{-- include modal here --}}
+                                @include('users.posts.contents.modals.delete')
+                            @else
+                                <form action = "#" method = "post">
+                                    @csrf
+
+                                    <button type="submit" class="border-0 bg-transparent p-0 text-primary">Follow</button>
+                                </form>
+                            @endif
                         </div>
                     </div>
                 </div>
 
-                <div class="card-body w-100 bg-white"></div>
+                {{-- DETAILS --}}
+                <div class = "card-body w-100 bg-white">
+                    {{-- heart button + No. of likes + categories --}}
+                    <div class = "row align-items-center">
+                        {{-- heart button --}}
+                        <div class = "col-auto">
+                            {{-- like post --}}
+                            <form action = "#" method = "post">
+                                @csrf
+                                <button type = "submit" class = "btn btn-sm shadow-none p-0">
+                                    <i class="fa-regular fa-heart"></i>
+                                </button>
+                            </form>
+                        </div>
+
+                        {{-- No. of likes --}}
+                        <div class = "col-auto px-0">
+                            <span>3</span>
+                        </div>
+
+                        {{-- categories --}}
+                        <div class = "col text-end">
+                            @forelse ($post->categoryPost as $category_post)
+                                <span class = "badge bg-secondary bg-opacity-50">
+                                    {{ $category_post->category->name }}
+                                </span>
+                            @empty
+                                <div class="badge bg-dark text-wrap">Uncategorized</div>
+                            @endforelse
+                        </div>
+                    </div>
+
+                    {{-- owner + description --}}
+                    <a href = "#" class = "text-decoration-none text-dark fw-bold">
+                        {{ $post->user->name }}
+                    </a>
+                    &nbsp;
+                    <p class = "fw-light d-inline">{{ $post->description }}</p>
+                    <p class = "text-uppercase text-muted xsmall">{{ date('M d, Y', strtotime($post->created_at)) }}</p>
+
+                    {{-- form comment --}}
+
+                </div>
             </div>
         </div>
     </div>
