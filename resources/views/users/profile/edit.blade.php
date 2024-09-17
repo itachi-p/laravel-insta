581c6967ec66dd58b7525d019501e7d07bea7f75
itachi-p <itachip38@gmail.com>
2024-09-18 01:31:26 +0900

    Profile page edit screen is complete.

--- resources/views/users/profile/edit.blade.php
+++ resources/views/users/profile/edit.blade.php
@@ -14,22 +14,59 @@
 
                 {{-- avatar & upload image --}}
                 <div class="row mb-3">
+                    {{-- avatar --}}
+                    <div class = "col-4">
+                        @if ($user->avatar)
+                            <img src="{{ $user->avatar }}" alt="{{ $user->name }}"
+                                class="img-thumbnail rounded-circle d-block mx-auto avatar-lg">
+                        @else
+                            <i class="fa-solid fa-circle-user text-secondary d-block text-center icon-lg"></i>
+                        @endif
+                    </div>
 
+                    {{-- upload file --}}
+                    <div class = "col-auto align-self-end">
+                        <input type  = "file" name = "avatar" id = "avatar" class = "form-control form-control-sm mt-1"
+                            aria-describedby = "avatar-info">
+                        Acceptable formats: jpeg, jpg, png, gif only<br>
+                        Max file size is 1024kb
+                    </div>
+                    {{-- Error --}}
+                    @error('avatar')
+                        <p class="text-danger small">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 {{-- name --}}
-                <div class="mb-3">
-
+                <div class = "mb-3">
+                    <label for   = "name" class = "form-label fw-bold">Name</label>
+                    <input type  = "text" name  = "name" id = "name" value = "{{ old('name', $user->name) }}"
+                        class = "form-control" autofocus>
+                    {{-- Error --}}
+                    @error('name')
+                        <p class="text-danger small">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 {{-- email --}}
-                <div class="mb-3">
-
+                <div class = "mb-3">
+                    <label for   = "email" class = "form-label fw-bold">E-Mail Address</label>
+                    <input type  = "text" name   = "email" id = "email" value = "{{ old('email', $user->email) }}"
+                        class = "form-control">
+                    {{-- Error --}}
+                    @error('email')
+                        <p class="text-danger small">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 {{-- introduction --}}
-                <div class="mb-3">
-
+                <div class = "mb-3">
+                    <label for   = "introduction" class = "form-label fw-bold">Introduction</label>
+                    <textarea name="introduction" id="introduction" rows="5" placeholder="Describe yourselef" class="form-control">{{ old('introduction', $user->introduction) }}</textarea>
+                    {{-- Error --}}
+                    @error('introduction')
+                        <p class = "text-danger small">{{ message }}</p>
+                    @enderror
                 </div>
 
                 <button type="submit" class="btn btn-warning px-5">Save</button>
