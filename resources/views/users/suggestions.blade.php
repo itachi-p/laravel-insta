18c591aaf628d1fe9d4fa3e412d3bcdb03f6764f
itachi-p <itachip38@gmail.com>
2024-09-22 11:13:06 +0900

    feat: Enhance suggested users display with avatars and profile links

--- resources/views/users/suggestions.blade.php
+++ resources/views/users/suggestions.blade.php
@@ -3,7 +3,19 @@
 @section('title', 'Suggested Users')
 
 @section('content')
-    @foreach($suggested_users as $user)
-        <div>{{ $user->name }}</div>
+@foreach($suggested_users as $user)
+{{-- Avatar --}}
+<div class="row">
+    <div class="col-auto">
+        <a href="{{ route('profile.show', $user->id) }}">
+            @if ($user->avatar)
+            <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="rounded-circle avatar-sm">
+            @else
+            <i class="fa-solid fa-circle-user text-secondary icon-sm"></i>
+            @endif
+        </a>
+
+    </div>
+    <div>{{ $user->name }}</div>
     @endforeach
-@endsection
+    @endsection
