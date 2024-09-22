69c3b662fd29b32f5ffbeb5366e3e3b76a589719
itachi-p <itachip38@gmail.com>
2024-09-22 23:55:24 +0900

    feat: Update user list to include activation and deactivation options for soft deleted users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,64 +3,74 @@
 @section('title', 'Admin: Users')
 
 @section('content')
-    {{-- Show all users here --}}
-    <table class = "table table-hover align-middle bt-white border text-secondary">
-        <thead class = "small table-success text-secondary">
-            <tr>
-                <th></th>
-                <th>NAME</th>
-                <th>EMAIL</th>
-                <th>CREATED_AT</th>
-                <th>STATUS</th>
-                <th></th>
-            </tr>
-        </thead>
+{{-- Show all users here --}}
+<table class="table table-hover align-middle bt-white border text-secondary">
+    <thead class="small table-success text-secondary">
+        <tr>
+            <th></th>
+            <th>NAME</th>
+            <th>EMAIL</th>
+            <th>CREATED_AT</th>
+            <th>STATUS</th>
+            <th></th>
+        </tr>
+    </thead>
 
-        <tbody>
-            @foreach ($all_users as $user)
-                <tr>
-                    <td>
-                        @if ($user->avatar)
-                            <img src="{{ $user->avatar }}" alt="" class="rounded-circle d-block mx-auto avatar-md">
-                        @else
-                            <i class="fa-solid fa-circle-user text-center icon-md d-block"></i>
-                        @endif
-                    </td>
-                    <td>
-                        <a href = "{{ route('profile.show', $user->id) }}" class = "text-decoration-none text-dark fw-bold">
-                            {{ $user->name }}
-                        </a>
-                    </td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ date('M d, Y', strtotime($user->created_at)) }}</td>
-                    <td>
-                        {{-- $user->trashed() returns TRUE if the user was soft deleted. --}}
+    <tbody>
+        @foreach ($all_users as $user)
+        <tr>
+            <td>
+                @if ($user->avatar)
+                <img src="{{ $user->avatar }}" alt="" class="rounded-circle d-block mx-auto avatar-md">
+                @else
+                <i class="fa-solid fa-circle-user text-center icon-md d-block"></i>
+                @endif
+            </td>
+            <td>
+                <a href="{{ route('profile.show', $user->id) }}" class="text-decoration-none text-dark fw-bold">
+                    {{ $user->name }}
+                </a>
+            </td>
+            <td>{{ $user->email }}</td>
+            <td>{{ date('M d, Y', strtotime($user->created_at)) }}</td>
+            <td>
+                {{-- $user->trashed() returns TRUE if the user was soft deleted. --}}
+                @if ($user->trashed())
+                <i class="fa-regular fa-circle text-secondary"></i>&nbsp;Inactive
+                @else
+                <i class="fa-solid fa-circle text-success"></i>&nbsp;Active
+                @endif
+
+            </td>
+            <td>
+                @if (Auth::user()->id != $user->id)
+                <div class="dropdown">
+                    <button class="btn btn-sm" data-bs-toggle="dropdown">
+                        <i class="fa-solid fa-ellipsis"></i>
+                    </button>
+
+                    <div class="dropdown-menu">
                         @if ($user->trashed())
-                            <i class="fa-regular fa-circle text-secondary"></i>&nbsp;Inactive
+                        <button class="dropdown-item text-success" data-bs-toggle="modal"
+                            data-bs-target="#activate-user-{{ $user->id }}">
+                            <i class="fa-solid fa-user-check"></i> Activate {{ $user->name }}
+                        </button>
                         @else
-                            <i class="fa-solid fa-circle text-success"></i>&nbsp;Active
+                        <button class="dropdown-item text-danger" data-bs-toggle="modal"
+                            data-bs-target="#deactivate-user-{{ $user->id }}">
+                            <i class="fa-solid fa-user-slash"></i> Deactivate {{ $user->name }}
+                        </button>
                         @endif
-                    </td>
-                    <td>
-                        @if (Auth::user()->id != $user->id)
-                            <div    class = "dropdown">
-                            <button class = "btn btn-sm" data-bs-toggle = "dropdown">
-                                    <i class="fa-solid fa-ellipsis"></i>
-                                </button>
 
-                                <div    class = "dropdown-menu">
-                                    <button class = "dropdown-item text-danger" data-bs-toggle = "modal" data-bs-target="#deactivate-user-{{ $user->id }}">
-                                        <i class="fa-solid fa-user-slash"></i> Deactivate {{ $user->name }}
-                                    </button>
-                                </div>
-                            </div>
-                            {{-- Include Deactivate User Modal here --}}
-                            @include('admin.users.modals.status')
-                        @endif
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $all_users->links() }}
+                    </div>
+                </div>
+                {{-- Include Deactivate User Modal here --}}
+                @include('admin.users.modals.status')
+                @endif
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+{{ $all_users->links() }}
 @endsection
