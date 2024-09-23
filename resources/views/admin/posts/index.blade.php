1979a230b59210f2e5f518dad412a2a783a3d253
itachi-p <itachip38@gmail.com>
2024-09-23 14:30:54 +0900

    feat: Enhance admin posts index view with post ID, image, and creation date

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 {{-- Show all posts here --}}
 <table class="table table-hover align-middle bt-white border text-secondary">
-    <thead class="small table-success text-secondary">
+    <thead class="small table-primary text-secondary">
         <tr>
             <th></th>
             <th></th>
@@ -20,6 +20,20 @@
     <tbody>
         @foreach ($all_posts as $post)
         <tr>
+            <td>
+                {{ $post->id }}
+            </td>
+            <td>
+                <img src="{{ $post->image }}" alt="{{ $post->name }}" class="d-block mx-auto image-lg">
+            </td>
+            <td>
+                {{-- CATEGORY --}}
+                <a href="{{ route('profile.show', $post->id) }}" class="text-decoration-none text-dark fw-bold">
+                    {{ $post->name }}
+                </a>
+            </td>
+            <td>{{ $post->user->name }}</td>
+            <td>{{ date('M d, Y', strtotime($post->created_at)) }}</td>
 
         </tr>
         @endforeach
