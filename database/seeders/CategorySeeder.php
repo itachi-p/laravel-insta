492c8119c7b6c8e287e287642942dc277aed95e5
itachi-p <itachip38@gmail.com>
2024-09-24 20:32:48 +0900

    feat: Add CategorySeeder and AdminSeeder for initial database seeding

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CategorySeeder extends Seeder
 {
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $categories = [
+            ['name' => 'Instrument', 'created_at' => NOW(), 'updated_at' => NOW()],
+            ['name' => 'Car', 'created_at' => NOW(), 'updated_at' => NOW()],
+        ];
+
+        $this->category->insert($categories);
     }
 }
