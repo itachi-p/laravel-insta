41d5d2ab7f268ac7a632e80d691342ee99acae40
itachi-p <itachip38@gmail.com>
2024-09-24 21:15:14 +0900

    feat: Enhance user search view with profile links and follow functionality

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -13,17 +13,43 @@
             <div class = "row align-items-center mb-3">
                 {{-- avatar --}}
                 <div class = "col-auto">
-
+                <a   href  = "{{ route('profile.show', $user->id) }}">
+                        @if ($user->avatar)
+                            <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="rounded-circle avatar-md">
+                        @else
+                            <i class="fa-solid fa-circle-user text-secondary icon-md"></i>
+                        @endif
+                    </a>
                 </div>
 
                 {{-- name + email --}}
                 <div class = "col ps-0 text-truncate">
-
+                <div class = "col ps-0 text-truncate">
+                <a   href  = "{{ route('profile.show', $user->id) }}" class = "text-decoration-none text-dark fw-bold">
+                            {{ $usesr->name }}
+                        </a>
+                        <p class="text-muted mb-0">{{ $user->email }}</p>
+                    </div>
                 </div>
 
                 {{-- button follow/following --}}
                 <div class = "col-auto">
+                    @if ($user->id !== Auth::usesr()->id)
+                        @if ($user->isFollowed())
+                            <form action = "{{ route('follow.destroy', $user->id) }}" method = "post">
+                                @csrf
+                                @method('DELETE')
 
+                                <button type="submit" class="btn btn-outline-secondary fw-bold btn-sm">Following</button>
+                            </form>
+                        @else
+                            <form action = "{{ route('follow.store', $user-id) }}" method = "post">
+                                @csrf
+
+                                <button type="submit" class="btn btn-primary fw-bold btn-sm">Follow</button>
+                            </form>
+                        @endif
+                    @endif
                 </div>
             </div>
         @empty
